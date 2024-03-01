50d75d655b4d915bb3ae40bdc9a8f14bbed2bd36
AdebayoMarzouq <marzouqdevmail@gmail.com>
2024-03-01 16:10:52 +0100

    Added writelog to see fetch data

--- app/functions/analyzeComment.ts
+++ app/functions/analyzeComment.ts
@@ -82,6 +82,7 @@ class AnalyzeComment {
 
 			for (const dt of collection) {
 				const { error, data } = dt;
+				writeLog("processed_data.log", `${new Date().toLocaleString()}: ${JSON.stringify(dt)}\n`);
 				if (!error) {
 					if ((data as CommentData).user_id && (data as CommentData).points) {
 						writePoint((data as CommentData).user_id, (data as CommentData).points);
